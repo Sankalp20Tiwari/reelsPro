e9f96eacbf3b27601c134cf38037d36b09f1f63c
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-02-23 23:43:21 +0530

    dashboard and video display

---
+++ app/(app)/dashboard/page.tsx
@@ -0,0 +1,59 @@
+"use client"
+import { SectionHeading } from '@/app/components/SectionHeading';
+import VideoRenderCard from '@/app/components/VideoRenderCard';
+import { apiClient } from '@/lib/api-client';
+import { IVideo } from '@/models/Video';
+import { Loader2 } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+
+const DashboardPage = () => {
+  const [videos, setVideos] = useState<IVideo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const data = await apiClient.getVideos();
+        setVideos(data);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }finally{
+        setLoading(false)
+      }
+    };
+
+    fetchVideos();
+  }, []);
+  return (
+    <div className='max-w-7xl mx-auto pt-20 px-4 '>
+      <div className='flex flex-col gap-4 items-center justify-center'>
+      <SectionHeading heading="All Videos" />
+      <p className='text-white hidden md:block'>"Discover new content, explore diverse genres, and enjoy a community-driven platform for sharing and viewing videos."</p>
+      </div>
+      {
+        loading ? (
+          <Loader2 className='animate-spin' />
+        ) : (
+            <>
+              {
+                videos.length === 0 ?(
+                  <div className='flex flex-col gap-4 items-center mt-12 justify-center'>
+                    <p className='text-white text-3xl'>No videos found</p>
+                  </div>
+                  
+                ) :(
+                  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 gap-4'>
+                    {videos.map((video) => (
+                        <VideoRenderCard video={video} key={video._id?.toString()} />
+                    ))}
+                  </div>
+                )
+              }
+            </>
+        )
+      }
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/components/VideoRenderCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { IVideo } from '@/models/Video'
+import { IKVideo } from "imagekitio-next";
+  
+
+const VideoRenderCard = ({video} : {video : IVideo}) => {
+
+  return (
+        <Card >
+        <CardContent>
+        <div  className="rounded-xl overflow-hidden relative mx-auto h-full w-full"
+            style={{ aspectRatio: "9/16" }}>
+        <IKVideo
+            path={video.videoUrl}
+            transformation={[{ height: "1920", width: "1080" }]}
+            controls={video.controls}
+            className='w-full h-full object-cover'
+        />
+        </div>
+        <div>
+        <h2 className='text-xl text-white capitalize'>{video.title}</h2>
+        <p className='text-sm text-gray-200 text-muted-foreground'>{video.description}</p>
+        </div>
+        </CardContent>
+        </Card>
+
+  )
+}
+
+export default VideoRenderCard
+
+
